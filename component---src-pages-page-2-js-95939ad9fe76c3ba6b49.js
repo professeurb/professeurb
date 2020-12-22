506685fe25e89545a0a492aa0d562f8bb1f0c32e
Olivier <olivier@Albert.local>
2020-12-22 11:43:11 +0100

    Updates

---
+++ component---src-pages-page-2-js-95939ad9fe76c3ba6b49.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{1130:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),c=a(137),l=a(1125);t.default=function(){return o.a.createElement(l.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-95939ad9fe76c3ba6b49.js.map
